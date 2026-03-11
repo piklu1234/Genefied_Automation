98e126dc86f03638c25d66837ddd43315bc51c81
ganeshkr3810 <ganeshkr3810@gmail.com>
2026-03-11 16:03:13 +0530

    new setup

---
+++ src/main/java/com/genefied/nonSAAS/Genefied/App.java
@@ -0,0 +1,10 @@
+package com.genefied.nonSAAS.Genefied;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
